ef76bb31e3b99a83cae58fa44d017a76e1ff303e
mjanez <96422458+mjanez@users.noreply.github.com>
2024-09-16 11:30:22 +0200

    Update package/resource forms to Tabs
    Metadata creation/editing forms now use tabs instead of steps, allowing all metadata elements to be set as mandatory and greatly improving the interaction and usability of the interface.

--- ckanext/schemingdcat/assets/js/modules/schemingdcat-form-tabs.js
+++ ckanext/schemingdcat/assets/js/modules/schemingdcat-form-tabs.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
+    // Get the csrf value from the page meta tag
+    var csrf_value = $('meta[name=_csrf_token]').attr('content');
+    // Create the hidden input
+    var hidden_csrf_input = $('<input name="_csrf_token" type="hidden" value="' + csrf_value + '">');
+
+    // Append CSRF token to all forms
+    $('form').each(function() {
+        hidden_csrf_input.clone().prependTo($(this));
+    });
+
     // Initialize the first tab and pane as active
     $("#groupTab .nav-pills li:first").addClass("active");
-    $("#groupTab .tab-pane:first").addClass("active fade in");
+    $("#groupTab .nav-pills li:first .nav-link").addClass("active");
+    $("#groupTab .tab-pane:first").addClass("active show");
 
     // Show the form group list of the first tab
     $("#groupTab .nav-pills li:first .form-group-list").show();
 
     // Move forward
     $("#next-tab").on('click', function(e) {
+        e.preventDefault();
         var $activeTab = $('.dataset-form .nav > .active');
         var $nextTab = $activeTab.next('li');
-        
+
         if ($nextTab.length > 0) {
-            $nextTab.find('a').trigger('click');
-            // Removed the animate function
+            $nextTab.find('a').tab('show'); // Use .tab('show') to activate the next tab
             window.scrollTo(0, $nextTab.offset().top);
         }
 
@@ -22,12 +33,12 @@ $(document).ready(function() {
             $('#next-tab').hide();
         }
 
-        e.preventDefault();
         return false;
     });
 
     // Show/hide form group list on tab click
     $(".tab-link").on('click', function(e) {
+        e.preventDefault();
         var $formGroupList = $(this).siblings(".form-group-list");
         $(".form-group-list").not($formGroupList).slideUp();
         $formGroupList.slideToggle();
@@ -48,7 +59,7 @@ $(document).ready(function() {
     // Remove duplicate form groups
     var seenFormGroups = new Set();
     $("#groupTab .tab-pane .card").each(function() {
-        var formGroupId = $(this).data("form_group_id");
+        var formGroupId = $(this).data("bs-form_group_id");
         if (seenFormGroups.has(formGroupId)) {
             $(this).remove();
         } else {
