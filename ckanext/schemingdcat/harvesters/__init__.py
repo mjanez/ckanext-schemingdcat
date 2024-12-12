66f4ad15d684e94d3b25be58c41f6540196e7c1c
mjanez <96422458+mjanez@users.noreply.github.com>
2024-12-12 11:38:20 +0100

    Add CSW harvester to the list of available harvesters

--- ckanext/schemingdcat/harvesters/__init__.py
+++ ckanext/schemingdcat/harvesters/__init__.py
@@ -2,5 +2,6 @@ from ckanext.schemingdcat.harvesters.base import SchemingDCATHarvester
 from ckanext.schemingdcat.harvesters.ckan import SchemingDCATCKANHarvester
 from ckanext.schemingdcat.harvesters.xls import SchemingDCATXLSHarvester
 from ckanext.schemingdcat.harvesters.sql.postgres import SchemingDCATPostgresHarvester
+from ckanext.schemingdcat.harvesters.csw import SchemingDCATCSWHarvester
 
-__all__ = ['SchemingDCATHarvester', 'SchemingDCATCKANHarvester', 'SchemingDCATXLSHarvester', 'SchemingDCATPostgresHarvester']
+__all__ = ['SchemingDCATHarvester', 'SchemingDCATCKANHarvester', 'SchemingDCATXLSHarvester', 'SchemingDCATPostgresHarvester', 'SchemingDCATCSWHarvester']
